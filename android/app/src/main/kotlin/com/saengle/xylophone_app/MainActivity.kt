eec38228358546a756d1b2b043c294b62ed08ccf
saengle <zohv817@gmail.com>
2023-01-30 17:43:15 +0900

    Start Xylophone project

---
+++ android/app/src/main/kotlin/com/saengle/xylophone_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.saengle.xylophone_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/xylophone.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Xlyophone extends StatelessWidget {
+  const Xlyophone({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
